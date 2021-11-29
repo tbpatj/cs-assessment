b2c0f2127c313905bd4202295dacb983d30cc1a7
tbpatj <timothy@Timothys-MBP.localdomain>
2021-11-29 11:58:34 -0700

    added the pangram

--- stepTwo.js
+++ stepTwo.js
@@ -7,7 +7,7 @@ function addToZero(arr){
     }
     return result === 0 ? true : false;
 }
-console.log(addToZero([1,2,3,4,-0]));
+console.log("adds to zero " + addToZero([1,2,3,4,-0]));
 
 //Time complexity is O(n)
 //Space complexity O(n)
@@ -16,18 +16,33 @@ function hasUniqueChars(str){
     let i = 0;
     let l = {};
     while(!l[str[i]] && i < str.length){
-        console.log(l);
         l[str[i]] = i;
         i++;
     }
     if(i === str.length) return true;
     else return false;
-    
 }
+    
 
-console.log(hasUniqueChars("Moonday"));
+console.log("has unique chars " + hasUniqueChars("Moonday"));
 //This function would probably be a O(n)
 //space complexity O(n)
+
+
+function isPangram(input){
+    let set = new Set();
+    for(let i = 0; i < input.length; i ++){
+        if(input.charCodeAt(i) <= 122 && input.charCodeAt(i) >= 97){
+            set.add(input[i]);
+        }
+    }
+    if(set.size == 26)return true;
+    else return false
+}
+//isPangram('abcdefghijklmnopqrstuvwxyz');
+console.log("is pangram " + isPangram('The quick brown fox jumps over the lazy dog!'));
+//o(n)
+//space complexity o(n)
 
 function findLongestWord(arr){
     let longest = {length: 0, index: -1}
@@ -40,7 +55,7 @@ function findLongestWord(arr){
     return longest.length;
 }
 
-console.log(findLongestWord(["hi", "hello"]));
+console.log("find longest word " + findLongestWord(["hi", "hello"]));
 
 //O(n) due to it will always run the entire list, so thus it will get harder and harder for it to push through 
 //space complexity O(n)
